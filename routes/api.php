bfdf9b318e7738098ad50bbe7ef556f56c3dcfd7
kim <tygh987654@gmail.com>
2023-03-06 15:38:32 +0800

    新增匯率計算API

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('quiz/exchange',function(Request $request){
+    if((!$request->from || !$request->to)){
+        return false;
+    }
+
+    $allow_currency = collect(['USD','TWD','JPY']);
+    $from = strtoupper((string)$request->from);
+    $to = strtoupper((string)$request->to);
+    if(($request->from == $request->to) ||  !$allow_currency->contains($from) || !$allow_currency->contains($to)){
+        return flase;//參數錯誤
+    }
+
+    $content = Http::get('https://tw.rter.info/capi.php');
+    $result = $content->json($from.$to);
+    return response()->json([
+                                'exchange_rate' => (float)$result['Exrate'],
+                                'updated_at'    => (string)$result['UTC']
+                            ]);
+});
+
